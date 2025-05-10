53e12ba1bca70fe2b6516953cab681626e00e67b
mercysound <adekunleadewolemercy@gmail.com>
2025-05-10 19:46:00 +0100

    done

--- admin-banner/vite.config.js
+++ admin-banner/vite.config.js
@@ -1,12 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    host:'0.0.0.0',
-    port: 4100
-  },
+  base: '/admin/',  //  THIS IS CRUCIAL
 })
